c1a8904204cde4ab96247c299bfe0b878058f444
haripriyakamaraj2410 <haripriyakamaraj2410@gmail.com>
2026-04-09 19:29:08 +0530

    Refactor factorial input to use command line args

--- factorial.py
+++ factorial.py
@@ -1,8 +1,11 @@
+import sys
+
 def factorial(n):
     if n == 0 or n == 1:
         return 1
-    else:
-        return n * factorial(n - 1)
+    return n * factorial(n - 1)
 
-num = int(input("Enter a number: "))
-print(f"Factorial of {num} is {factorial(num)}")
+if __name__ == "__main__":
+    # Check if a number was passed as an argument, else default to 5
+    num = int(sys.argv[1]) if len(sys.argv) > 1 else 5
+    print(f"Factorial of {num} is {factorial(num)}")
